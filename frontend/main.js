e02162af3a5314e4e13722316b410de3f9856560
Arvind Kumar <aarviiyadav19@gmail.com>
2024-11-28 00:36:03 +0530

    url updated

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector('#visitorCount')
 
 async function updateCounter() {
-    const response = await fetch("https://ru67ppbfa5mhgp2fabypvjvar40afypi.lambda-url.ap-south-1.on.aws/")
+    const response = await fetch("https://ru67ppbfa5mhgp2fabypvjvar40afypi.lambda-url.ap-south-1.on.aws")
     const data = await response.json()
     counter.innerHTML = `views: ${data}`
 }
